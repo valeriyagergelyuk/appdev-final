03d63b93d0f5d15dd4e5dbe2c9f5565b85cbaa5f
%usernames% <%email%>
2023-11-23 21:50:44 -0500

    created item and user classes

---
+++ SmallBusiness/SmallBusiness/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallBusiness
+{
+    //i think this class will become useless after we create the database... but just for now
+    internal class Account
+    {
+
+        //instance variables
+        private String email;
+        private String password;
+        private String username;
+
+    }
+}
--- SmallBusiness/SmallBusiness/MainForm.Designer.cs
+++ SmallBusiness/SmallBusiness/MainForm.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.loginBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // loginBtn
             // 
-            this.button1.BackColor = System.Drawing.Color.IndianRed;
-            this.button1.Font = new System.Drawing.Font("Bodoni MT Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(894, -1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(197, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Login / Create Account";
-            this.button1.UseVisualStyleBackColor = false;
+            this.loginBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.loginBtn.Font = new System.Drawing.Font("Bodoni MT Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginBtn.Location = new System.Drawing.Point(894, -1);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(197, 58);
+            this.loginBtn.TabIndex = 0;
+            this.loginBtn.Text = "Login / Create Account";
+            this.loginBtn.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1091, 624);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loginBtn);
             this.Name = "MainForm";
             this.Text = "Small Business";
             this.ResumeLayout(false);
@@ -57,7 +57,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loginBtn;
     }
 }
 
